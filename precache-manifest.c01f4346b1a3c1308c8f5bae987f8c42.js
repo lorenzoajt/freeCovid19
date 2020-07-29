0cc6cec9e0b412402315d98a61d71b53f85178b1
lorenzoajt <lorenzoajt@gmail.com>
2020-07-29 01:11:30 -0500

    Updates

---
+++ precache-manifest.c01f4346b1a3c1308c8f5bae987f8c42.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4d9278cf6154049b6fda1ff5733229b",
+    "url": "/freeCovid19/index.html"
+  },
+  {
+    "revision": "458a8e763ee066246b56",
+    "url": "/freeCovid19/static/css/2.3107b260.chunk.css"
+  },
+  {
+    "revision": "67eb308d4f118d7615e1",
+    "url": "/freeCovid19/static/css/main.3de7864e.chunk.css"
+  },
+  {
+    "revision": "458a8e763ee066246b56",
+    "url": "/freeCovid19/static/js/2.719ddb57.chunk.js"
+  },
+  {
+    "revision": "9663b94d6b2756b914f404a6907e60df",
+    "url": "/freeCovid19/static/js/2.719ddb57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67eb308d4f118d7615e1",
+    "url": "/freeCovid19/static/js/main.cf029f47.chunk.js"
+  },
+  {
+    "revision": "5b5b861e940ca25195ef",
+    "url": "/freeCovid19/static/js/runtime-main.3e64ada8.js"
+  },
+  {
+    "revision": "2651268211599d22fe8b3e214e62814a",
+    "url": "/freeCovid19/static/media/ICONO con circulo_Mesa de trabajo 1.26512682.png"
+  },
+  {
+    "revision": "63f34423fed2ba936f78cf9685d1e739",
+    "url": "/freeCovid19/static/media/apartment-contemporary-couch-curtains-275484.63f34423.jpg"
+  },
+  {
+    "revision": "2ed590ec611c34f7c13fef0e6369b509",
+    "url": "/freeCovid19/static/media/noImage.2ed590ec.jpeg"
+  }
+]);
