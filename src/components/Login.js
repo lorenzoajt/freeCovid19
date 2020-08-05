f2c0a4655c16a5528fb88f030a44825dd143f225
lorenzoajt <lorenzoajt@gmail.com>
2020-08-05 02:38:56 -0500

    log in button style

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,28 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import Button from '@material-ui/core/Button';
+import { ThemeProvider } from '@material-ui/core/styles';
+import {Theme} from './Theme.js'
+import { makeStyles } from '@material-ui/core/styles';
 
+
+const useStyles = makeStyles((theme) => ({  
+	  root: {
+	    margin: theme.spacing(2),
+	  },
+	}));
 const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
-
-  return <button onClick={() => loginWithRedirect()}>Iniciar Sesion</button>;
+  	const { loginWithRedirect } = useAuth0();
+	
+	
+	const classes = useStyles();
+	return (
+		<ThemeProvider theme={Theme}>
+	  	<Button className={classes.root} variant="contained" color="secondary" onClick={() => loginWithRedirect()}>
+			Iniciar Sesión
+		</Button>
+	</ThemeProvider>
+	)
 };
 
 export default LoginButton;
